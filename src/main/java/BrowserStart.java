cff563a06be84740c53d1cab547e3de87836a052
unknown <alexandrsm@ALEXANDRSM-NBW7.ld.yandex.ru>
2016-08-02 00:50:36 +0300

    first start and splash screen

--- src/main/java/BrowserStart.java
+++ src/main/java/BrowserStart.java
@@ -5,31 +5,45 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.net.URL;
+import java.util.concurrent.TimeoutException;
 
 
 public class BrowserStart {
-
     private AppiumDriver driver;
+
 
     @Before
     public void BeforeTest() throws Exception{
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("deviceName","aphone");
         capabilities.setCapability("appPackage", "com.yandex.browser");
-        capabilities.setCapability("appActivity", ".YandexBrowserActivity");
+        capabilities.setCapability("appActivity", ".YandexBrowserMainActivity");
         driver = new AndroidDriver(new URL("http://127.0.0.1:4444/wd/hub"), capabilities);
     }
 
-    @Test
 
+    @Test
     public void Test() throws Exception{
-        driver.findElement(By.id("bro_sentry_bar_fake"));
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("bro_sentry_bar_fake")));
+        }
+        catch (Exception s){
+            wait.until(ExpectedConditions.elementToBeClickable(By.id("activity_tutorial_close_button"))).click();
+        }
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("bro_sentry_bar_fake")));
     }
 
+    
     @After
-    public void tearDown() throws Exception {
+    public void After() throws Exception {
         driver.quit();
     }
 
 }
+
---
+++ src/main/java/SuggestTap.java
@@ -0,0 +1,6 @@
+/**
+ * Created by alexandrsm on 01.08.2016.
+ */
+public class SuggestTap {
+
+}
