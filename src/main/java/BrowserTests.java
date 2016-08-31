2ff49119e29e9b4c3edec6b1b6d3c73e5d761ef4
unknown <alexandrsm@ALEXANDRSM-NBW7.ld.yandex.ru>
2016-08-31 20:04:46 +0300

    .

--- src/main/java/BrowserTests.java
+++ src/main/java/BrowserTests.java
@@ -1,3 +1,4 @@
+import com.google.common.collect.Lists;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
@@ -8,7 +9,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
-import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -29,43 +29,53 @@ public class BrowserTests {
     }
 
 
-    @Test // Запуск браузера
+    @Test /* Запуск браузера */
     public void BrowserStart() throws Exception{
+        // Поиск омнибокса
         try {
             driver.findElement(By.id(("bro_sentry_bar_fake_text")));
         }
         catch (Exception e)
         {
-            WebElement tutorialCloseButton = driver.findElement(By.id("activity_tutorial_close_button"));
-            tutorialCloseButton.click();
+            // Если онибокс не найден, поиск кнопки закрытия туториала
+            try {
+                driver.findElement(By.id("activity_tutorial_close_button"));
+            }
+            // Если кнопка закрытия туториала не найдена, прохождение велком скрина
+            catch (Exception e1) {
+                WebElement welcomeScreenCheckbox = driver.findElement(By.id("import_checkbox_mark"));
+                welcomeScreenCheckbox.click();
+                WebElement welcomeScreenNextButton = driver.findElement(By.id("activity_import_next_button"));
+                welcomeScreenNextButton.click();
+            }
+            // Тап на кнопку закрытия туториала
+            finally {
+                WebElement tutorialCloseButton = driver.findElement(By.id("activity_tutorial_close_button"));
+                tutorialCloseButton.click();
+            }
         }
+        // Поиск омнибокса
         finally {
             driver.findElement(By.id(("bro_sentry_bar_fake_text")));
         }
     }
 
 
-    @Test // Тап по 3 элементу саджеста и ожидание загрузки
+    @Test /* Тап по 3 элементу саджеста и ожидание загрузки */
     public void SearchFromSuggest() throws Exception {
 
-        try {
-            driver.findElement(By.id(("bro_sentry_bar_fake_text")));
-        }
-        catch (Exception e)
-        {
-            WebElement tutorialCloseButton = driver.findElement(By.id("activity_tutorial_close_button"));
-            tutorialCloseButton.click();
-        }
-
+        // Старт браузера
+        BrowserStart();
+        // Тап в омнибокс
         WebElement omnibox = driver.findElement(By.id(("bro_sentry_bar_fake_text")));
         omnibox.click();
-
+        // Ввод в онибокс строки
         WebElement omniboxTextField = driver.findElement(By.id("bro_sentry_bar_input_edittext"));
         omniboxTextField.sendKeys("qwe");
-
-        List<WebElement> SuggestList = driver.findElements(By.id("bro_common_omnibox_text_layout"));
-        SuggestList.get(SuggestList.size()-3).click();
-
+        // Получение списка элементов саджеста и тап по 3 строке
+        List<WebElement> suggestList = driver.findElements(By.id("bro_common_omnibox_text_layout"));
+        Lists.reverse(suggestList).get(2).click();
+        // Вызов парсера логов (с передачей в него триггера, после которого будут получены логи)
         WebElement webView = driver.findElement(By.className("android.webkit.WebView"));
         logReader.FindString(driver, "Ya:ReportManager", "url opened", webView);
     }
--- src/main/java/LogReader.java
+++ src/main/java/LogReader.java
@@ -16,16 +16,33 @@ class LogReader {
     public void FindString(AppiumDriver driver, String logTag, String logString, WebElement trigger) throws Exception {
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         Pattern pattern = Pattern.compile(logTag + "(.*)" + logString);
+
+        // Поиск в логах, если триггер (android.webkit.WebView) найден (срабатывает в 90% случаев)
         if (trigger.isDisplayed()) {
-            logEntryList = driver.manage().logs().get("logcat").getAll();
-            for (LogEntry logEntry : logEntryList) {
-                if (logEntry.getTimestamp() > logTime.getTime()) {
-                    Matcher matcher = pattern.matcher(logEntry.getMessage());
-                    if (matcher.find())
-                        return;
+            if (Parser(driver, pattern)) {
+                return;
+            }
+        }
+        // Остальные 10%, при которых url opened логируется позже отображения триггера (android.webkit.WebView)
+        Thread.sleep(5000);
+        if (Parser(driver, pattern)) {
+            return;
+        }
+        throw new Exception(logString + " not found");
+    }
+
+    // Парсер логов (построчный перебор и сравнение с паттерном)
+    private boolean Parser(AppiumDriver driver, Pattern pattern) {
+        logEntryList = driver.manage().logs().get("logcat").getAll();
+        for (LogEntry logEntry : logEntryList) {
+            if (logEntry.getTimestamp() > logTime.getTime()) {
+                Matcher matcher = pattern.matcher(logEntry.getMessage());
+                if (matcher.find()) ;
+                {
+                    return true;
                 }
             }
-            throw new Exception(logString + " not found");
         }
+        return false;
     }
 }
